4029c3473cda90e6945b40f38ddd66442d15d6cd
Clément Gassmann-Prince <clementgassmannprince@gmail.com>
2024-09-06 18:26:31 -0400

    Create docs/conf.py

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+project = 'Palang'
+copyright = '2024, Clément Gassmann-Prince'
+author = 'Clément Gassmann-Prince'
+release = '0.1'
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'furo'
+html_static_path = ['_static']
