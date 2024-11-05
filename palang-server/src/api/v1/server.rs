a1d03877e428016b59350d71365e5414794627aa
Thinking-Dragon <clementgassmannprince@gmail.com>
2024-11-04 20:08:00 -0500

    Remove unused ServerArgs struct

--- palang-server/src/api/v1/server.rs
+++ palang-server/src/api/v1/server.rs
@@ -1,14 +1,3 @@
-pub struct ServerArgs {
-    pub host: String,
-    pub port: u16,
-}
-
-impl ServerArgs {
-    pub fn new(host: String, port: u16) -> Self {
-        ServerArgs { host, port }
-    }
-}
-
 pub struct AppState;
 
 impl AppState {
